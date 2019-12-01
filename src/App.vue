74d69441bf015f2721b2bc21dd31d365e0fd4147
ezexiel <ezequielmiranda87@gmail.com>
2019-12-01 21:20:22 +0100

    App cleaned

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <TodoList></TodoList>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
 export default {
   name: 'App',
+  components:{
+    TodoList
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
