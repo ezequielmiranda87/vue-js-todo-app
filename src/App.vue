cc4d4942dd5117b5ecea1e1a1666c038c97d6908
ezexiel <ezequielmiranda87@gmail.com>
2019-12-01 21:43:09 +0100

    Listing tasks

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="app">
-    <TodoList></TodoList>
+    <TodoList v-bind:todos="todos"></TodoList>
     <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList'
+
 export default {
   name: 'App',
   components:{
     TodoList
-  }
+  },
+   data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
 };
 </script>
 
 <style>
+
 #app {
 
 }
+
 </style>
